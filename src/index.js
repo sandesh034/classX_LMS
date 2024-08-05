d79f571cda74d3478a467710576337ae9783f6ac
sandesh034 <dhitalsandesh1@gmail.com>
2024-08-05 09:32:59 +0545

    vercel origin added

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const streamRouter = require('./routers/stream.router')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://classx.netlify.app'],
+    origin: ['http://localhost:5173', 'https://classx.netlify.app', 'https://class-x-frontend.vercel.app'],
     credentials: true
 }));
 app.use(cookieParser());
